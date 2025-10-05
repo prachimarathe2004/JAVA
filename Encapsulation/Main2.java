3b9ce41a490ef12edbfdceb953e5ece8999b5bcf
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-05 19:06:34 +0530

    Create Main2.java

---
+++ Encapsulation/Main2.java
@@ -0,0 +1,34 @@
+class BankAccount {
+    private double balance;
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Invalid withdrawal");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(5000);
+        account.withdraw(1500);
+        System.out.println("Current Balance: " + account.getBalance());
+    }
+}
