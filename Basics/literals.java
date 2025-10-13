57e9b564217258f34377ae9748e40a7af04af089
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-13 17:29:26 +0530

    Create literals.java

---
+++ Basics/literals.java
@@ -0,0 +1,34 @@
+public  class literals{
+    public static void main(String[]args){
+        int num1=0b101; //binary
+        int num2=0x7E; //hexadecimal
+        int num3=10_00_00_000;
+        double num4=56;
+        float num5=56;
+        double num6=12e10;
+        boolean num7=true;
+        char d='e';
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(num7);
+        System.out.println(d);
+
+
+
+        char c='a';
+        c++;
+        System.out.println(c);
+
+
+
+        
+        char s='8';
+        c++;
+        System.out.println(s);
+    }
+}
