db515f314089de8f1ee648240369de2a13acfe6c
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-13 17:01:31 +0530

    Create DataTypes.java

---
+++ Basics/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+    public static void main(String[] args) {
+     float marks = (float)6.5;
+     double num = 5.6;
+     boolean b=true;
+     long l=35364l;
+     char ch='p';
+     System.out.println(ch);
+     System.out.println(num);
+     System.out.println(marks);
+     System.out.println(b);
+     System.out.println(l);
+    }
+}
