8f466f4d640cb6cb048e7e9f1b619d7ac146d22e
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-14 16:51:49 +0530

    Create logical.java

---
+++ Basics/logical.java
@@ -0,0 +1,15 @@
+public class logical {
+    public static void main(String[] args) {
+        int x=7;
+        int y=5;
+        int a=5;
+        int b=9;
+
+        boolean result1= x>y || a<b;
+        boolean result2= x>y && a<b;
+        System.out.println(result1);
+        System.out.println(result2);
+
+    }
+}
+
