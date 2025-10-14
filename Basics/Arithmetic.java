62bd7406660eb3302b7c0b61882765d9f776d402
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-14 09:14:45 +0530

    Create  Arithmetic.java

---
+++ Basics/Arithmetic.java
@@ -0,0 +1,36 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int num1=7;
+        num1=num1+2;
+
+        System.out.println(num1);
+
+
+        num1+=3;
+        System.out.println(num1);
+
+        
+        num1-=3;
+        System.out.println(num1);
+
+        
+        num1*=3;
+        System.out.println(num1);
+
+        num1/=3;
+        System.out.println(num1);
+
+        num1++;
+        System.out.println(num1);
+
+         num1--;
+        System.out.println(num1);
+
+         ++num1;
+        System.out.println(num1);
+
+        --num1;
+        System.out.println(num1);
+       
+    }
+}
