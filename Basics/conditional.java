07983a7cdbb89674e9b2b7aa435dcd2e727f22b8
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-14 17:14:20 +0530

    Create conditional.java

---
+++ Basics/conditional.java
@@ -0,0 +1,15 @@
+public class conditional {
+    public static void main(String[] args) {
+        {
+            int x=168;
+
+            if(x>10 && x<=20){
+                    System.out.println("yess");
+            }else{
+                    System.out.println("NO");
+            }
+           
+        }
+    }
+    
+}
