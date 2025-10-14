6108acf1b1a56236689659fb22d6d0914f860f9b
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-14 09:52:30 +0530

    Create relational.java

---
+++ Basics/relational.java
@@ -0,0 +1,17 @@
+public class relational {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 6;
+
+        boolean result = (x == y);
+        System.out.println(result);  // true
+
+        boolean result1 = (x != y);
+        System.out.println(result1); // false
+
+        double s = 8.8;
+        double p = 9.8;
+        boolean result3 = (s <= p);
+        System.out.println(result3); // true
+    }
+}
