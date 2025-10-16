4757423ee6bf651c371aefd073d611bab743dd8d
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-16 10:42:50 +0530

    Create  NestedwhileLoop.java

---
+++ Basics/NestedwhileLoop.java
@@ -0,0 +1,19 @@
+public class NestedwhileLoop {
+    public static void main(String args[]){
+        
+        int i=1;
+        while(i<=4){
+        System.out.println("hi" +i);
+
+        int j=1;
+        while(j<=3){
+             System.out.println("hello" +j);
+             j++;
+        }
+        i++;
+
+        }
+        System.out.println("byee  " +i);
+        
+    }
+}
