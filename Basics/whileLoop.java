fc583aac7a3771d7b1c6e1bf86f21f8c933c736c
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-10-16 10:42:19 +0530

    Create whileLoop.java

---
+++ Basics/whileLoop.java
@@ -0,0 +1,11 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=4)
+        {
+        System.out.println("hii" + i);
+        i++;
+        }
+        System.out.println("byee" + i);
+    }
+}
